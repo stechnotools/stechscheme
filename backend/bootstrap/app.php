65aee11979d99d868a6d908aedf5c600ed72bbbd
niranjannsahoo <niranjannsahoo@gmail.com>
2026-04-29 13:23:49 +0530

    chenages

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->append(BlockBotRequests::class);
 
         $middleware->alias([
